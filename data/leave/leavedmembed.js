6474798b004bc76b54bd2a7c286db85999e8e78a
Picarita <84823885+Picarita@users.noreply.github.com>
2025-06-20 23:04:40 -0600

    Update leavedmembed.js

--- data/leave/leavedmembed.js
+++ data/leave/leavedmembed.js
@@ -3,8 +3,8 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = function createLeaveDMEmbed(member) {
     return new EmbedBuilder()
         .setColor('#FF9900')
-        .setTitle('👋 You left a server')
-        .setDescription(`Goodbye from **${member.guild.name}**.\nWe hope to see you again!`)
+        .setTitle('👋 Saliste del servidor')
+        .setDescription(`Te decimos adiós **${member.guild.name}**.\nEsperamos verte de nuevo!`)
         .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
         .setTimestamp();
 };
