7426c4282d6f6a78b7844f78e2a3a20b6dcfd579
Picarita <84823885+Picarita@users.noreply.github.com>
2025-07-10 21:42:20 -0600

    Update choose.js

--- commands/fun/choose.js
+++ commands/fun/choose.js
@@ -1,90 +1,64 @@
-/*
-
-☆.。.:*・°☆.。.:*・°☆.。.:*・°☆.。.:*・°☆
-                                                 
-  _________ ___ ___ ._______   _________    
- /   _____//   |   \|   \   \ /   /  _  \   
- \_____  \/    ~    \   |\   Y   /  /_\  \  
- /        \    Y    /   | \     /    |    \ 
-/_______  /\___|_  /|___|  \___/\____|__  / 
-        \/       \/                     \/  
-                    
-DISCORD :  https://discord.com/invite/xQF9f9yUEM                   
-YouTube : https://www.youtube.com/@GlaceYT                         
-
-Command Verified : ✓  
-Website        : ssrr.tech  
-Test Passed    : ✓
-
-☆.。.:*・°☆.。.:*・°☆.。.:*・°☆.。.:*・°☆
-*/
-
-
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { EmbedBuilder } = require('discord.js');
-const lang = require('../../events/loadLanguage');
+
+const mezclarPalabra = palabra => {
+    const letras = palabra.split('');
+    for (let i = letras.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [letras[i], letras[j]] = [letras[j], letras[i]];
+    }
+    return letras.join('');
+};
+
+const palabras = [
+    'manzana', 'banana', 'cereza', 'dátil', 'saúco', 'higo', 'uva', 'melón', 'kiwi', 'limón',
+    'mango', 'nectarina', 'naranja', 'papaya', 'membrillo', 'frambuesa', 'fresa', 'mandarina', 'ugli', 'vid',
+    'sandía', 'xigua', 'ñame', 'calabacín', 'aguacate', 'arándano', 'cantalupo', 'pitahaya', 'pomelo', 'arándano rojo',
+    'jaca', 'kiwano', 'lima', 'melón', 'néctar', 'aceituna', 'pera', 'ciruela', 'granada', 'ruibarbo',
+    'carambola', 'tomate', 'albaricoque', 'mora', 'coco', 'pepino', 'grosella', 'caqui', 'piña', 'tamarindo',
+    'durazno', 'maracuyá', 'berenjena', 'cebolla', 'pimiento', 'brócoli', 'zanahoria', 'coliflor', 'apio', 'maíz',
+    'espinaca', 'ajo', 'jengibre', 'col', 'lechuga', 'champiñón', 'calabaza', 'rábano', 'batata', 'nabo',
+    'remolacha', 'alcachofa', 'espárrago', 'frijol', 'coles', 'repollo', 'acelga', 'chile', 'diente de león', 'jalapeño',
+    'puerro', 'aceitunas', 'perejil', 'guisante', 'papa', 'chalote', 'arveja', 'brote', 'taro', 'berro',
+    'jícama', 'daikon', 'chayote', 'yogur', 'cheddar', 'mozzarella', 'parmesano', 'brie', 'feta', 'gouda',
+    'ricota', 'azul', 'cabra', 'requesón', 'crema', 'gruyere', 'provolone', 'suizo', 'havarti', 'colby'
+];
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('choose')
-        .setDescription(lang.chooseDescription)
-        .addStringOption(option =>
-            option.setName('options')
-                .setDescription(lang.chooseOptionsDescription)
-                .setRequired(true)),
-
+        .setName('palabrarevuelta')
+        .setDescription('Inicia un juego para adivinar una palabra revuelta.'),
     async execute(interaction) {
-        let sender = interaction.user;
-        let options;
+        const palabra = palabras[Math.floor(Math.random() * palabras.length)];
+        const palabraRevuelta = mezclarPalabra(palabra);
 
-        if (interaction.isCommand && interaction.isCommand()) {
-            options = interaction.options.getString('options').split(',');
-        } else {
-            const message = interaction;
-            sender = message.author;
-            const args = message.content.split(' ');
-            args.shift(); 
-            options = args.join(' ').split(',');
+        const embedJuego = new EmbedBuilder()
+            .setColor('#0099ff')
+            .setTitle('¡Adivina la Palabra!')
+            .setDescription(`Ordena las letras: **${palabraRevuelta}**`)
+            .setFooter({ text: 'Escribe tu respuesta abajo.' });
+
+        await interaction.reply({ embeds: [embedJuego] });
+
+        const filtro = respuesta => respuesta.author.id === interaction.user.id;
+
+        try {
+            const recolectado = await interaction.channel.awaitMessages({
+                filter: filtro,
+                max: 1,
+                time: 30000,
+                errors: ['time']
+            });
+
+            const respuestaUsuario = recolectado.first().content;
+
+            if (respuestaUsuario.toLowerCase() === palabra.toLowerCase()) {
+                await interaction.followUp('¡Correcto! 🎉');
+            } else {
+                await interaction.followUp(`❌ Incorrecto. La palabra era: **${palabra}**`);
+            }
+        } catch (error) {
+            await interaction.followUp('⏰ ¡Tardaste mucho en responder!');
         }
-
-        options = options.map(option => option.trim());
-
-        let chosenOption;
-        if (options.length === 1 && options[0].includes(' ')) {
-            options = options[0].split(' ');
-            chosenOption = options[Math.floor(Math.random() * options.length)];
-        } else {
-            chosenOption = options[Math.floor(Math.random() * options.length)];
-        }
-
-        const embed = new EmbedBuilder()
-            .setColor('#3498db')
-            .setTitle(lang.chooseTitle)
-            .setDescription(`${lang.chooseOptionsText} ${options.join(', ')}\n${lang.chooseChosenText} ${chosenOption}`)
-            .setTimestamp();
-
-        await interaction.reply({ embeds: [embed] });
     },
 };
-
-
-/*
-
-☆.。.:*・°☆.。.:*・°☆.。.:*・°☆.。.:*・°☆
-                                                 
-  _________ ___ ___ ._______   _________    
- /   _____//   |   \|   \   \ /   /  _  \   
- \_____  \/    ~    \   |\   Y   /  /_\  \  
- /        \    Y    /   | \     /    |    \ 
-/_______  /\___|_  /|___|  \___/\____|__  / 
-        \/       \/                     \/  
-                    
-DISCORD :  https://discord.com/invite/xQF9f9yUEM                   
-YouTube : https://www.youtube.com/@GlaceYT                         
-
-Command Verified : ✓  
-Website        : ssrr.tech  
-Test Passed    : ✓
-
-☆.。.:*・°☆.。.:*・°☆.。.:*・°☆.。.:*・°☆
-*/
