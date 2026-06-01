8c6a84ffacb22fb3d3560b33d6d0f22b72955c25
Picarita <84823885+Picarita@users.noreply.github.com>
2026-05-31 21:43:22 -0600

    Refactor lavalink configuration to a single node

--- lavalink.js
+++ lavalink.js
@@ -1,28 +1,9 @@
-const config = require('./config.json');
-
 module.exports = {
     enabled: true,
-    lavalink: config.nodes || [
-        {
-            name: "Node 1",
-            host: "lavalink.jirayu.net",
-            port: 13592,
-            password: "youshallnotpass",
-            secure: false
-        },
-        {
-            name: "Node 2",
-            host: "lavalinkv4.serenetia.com",
-            port: 80,
-            password: "https://seretia.link/discord",
-            secure: false
-        },
-        {
-            name: "Node 3",
-            host: "lavalink.triniumhost.com",
-            port: 2333,
-            password: "kirito",
-            secure: false
-        }
-    ]
+    lavalink: {
+        host: "lavalink.jirayu.net",
+        port: 13592,
+        password: "youshallnotpass",
+        secure: false
+    }
 };
