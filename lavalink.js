820297e87b6de8e70a0aa428f01989b364762284
Picarita <84823885+Picarita@users.noreply.github.com>
2026-05-31 21:31:04 -0600

    Refactor lavalink configuration to use nodes from config

--- lavalink.js
+++ lavalink.js
@@ -1,11 +1,28 @@
+const config = require('./config.json');
 
 module.exports = {
-    enabled: true, 
-    lavalink: {
-      name: "GlaceYT",
-      password: "glaceyt",
-      host: "5.39.63.207",
-      port:  8262,
-      secure: false
-    }
+    enabled: true,
+    lavalink: config.nodes || [
+        {
+            name: "Node 1",
+            host: "lavalink.jirayu.net",
+            port: 13592,
+            password: "youshallnotpass",
+            secure: false
+        },
+        {
+            name: "Node 2",
+            host: "lavalinkv4.serenetia.com",
+            port: 80,
+            password: "https://seretia.link/discord",
+            secure: false
+        },
+        {
+            name: "Node 3",
+            host: "lavalink.triniumhost.com",
+            port: 2333,
+            password: "kirito",
+            secure: false
+        }
+    ]
 };
